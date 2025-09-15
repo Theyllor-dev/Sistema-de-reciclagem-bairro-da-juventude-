1fb1331620d7f7f272721b1835e79f65396116b3
Theyllor maciel de souza <theyllormaciel0@gmail.com>
2025-09-15 19:28:10 -0300

    Add files via upload

---
+++ Projeto.BairrodaJuventude/script/script.js
@@ -0,0 +1,33 @@
+var verificarBtn = document.getElementById("verifica");
+var text = document.getElementById("text");
+var imagem = document.getElementById("imagem");
+
+// objeto que guarda chave(itens) e seus valores (cor da lixeira, imagem, cor do texto)
+var itens = {
+    garrafa: ["vermelha", "lixovermelho.png", "red"],
+    plasticos: ["vermelha", "lixovermelho.png", "red"],
+    metal: ["amarela", "lixoamarelo.png", "#e7dd00"],
+    papel: ["azul", "lixoazul.png", "blue"],
+    vidro: ["verde", "lixoverde.png", "green"],
+};
+
+verificarBtn.addEventListener("click", function () {
+    var item = document.getElementById("item").value.toLowerCase(); 
+    var chaves = Object.keys(itens);
+    var valores = Object.values(itens);
+
+    // percorre a lista de itens cadastrados
+    for (let i = 0; i < chaves.length; i++) {
+        if (item === chaves[i]) {
+            text.textContent = "Lixeira de cor: " + valores[i][0]; 
+            text.style.color = valores[i][2]; 
+            imagem.src = valores[i][1]; 
+            return;
+        }
+    }
+
+    // caso não encontre
+    text.textContent = "Item não cadastrado";
+    text.style.color = "black";
+    imagem.src = "";
+});
